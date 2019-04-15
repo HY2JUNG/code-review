97ed08dda0173e78ddd71bfda975100291ed5d12
hyjung2 <dud022@naver.com>
2019-04-15 15:41:27 +0900

    실습 commit
    오래된 URL을 새로운 URL 패턴으로 교체하는 Redirect 컴포넌트를 완성하세요.

--- projects/App.jsx
+++ projects/App.jsx
@@ -1,67 +1,29 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, NavLink} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 import './App.css';
 
-const isActiveLink = (match, location) => {
-    console.log(match, location, location.search);
-
-    if (location.search && /number=(\d{2})/.test(location.search)) return true;
-    return false;
-};
-
-const Links = () => (
-
+const Links =() => (
     <nav>
-        <NavLink exact activeClassName="active" to="/">
-            Home
-        </NavLink>
-        <NavLink activeStyle={{ color :'green'}} to={{pathname:'/about'}}>
-            About
-        </NavLink>
-        <NavLink isActive={isActiveLink} activeClassName="active" replace to="/contact?number">
-            Contact
-        </NavLink>
+        <Link to="/">Home</Link>
+        <Link to="/old">Old</Link>
+        <Link to="/new">New</Link>
     </nav>
+
 );
 
-/*
-const App = () => (
-    <Router>
-        <div>
-            <Links/>
-            <Route path="/:page/:subpage"
-                render={({match}) => {
-                console.log(match);
-                return (
-                    <div>
-                        <h1>
-                            PAGE:, {match.params.page || 'Home'} <br/>
-                            SUB page : {match.params.subpage || 'Sub'} <br/>
-                        </h1>
-                    </div>
-                )
-            }}
-            />
-        </div>
-    </Router>
-);
-*/
 
 const App = () => (
     <Router>
         <div>
-            <Links/>
-            <Route path="/:date(\d{4}-\d{2}-\d{2}):ext(\.[a-z]+)"
-                render={({match}) => (
-                    <>
-                        <h1>YES Matched!</h1>
-                        <div> date: {match.params.date}</div>
-                        <div> ext: {match.params.ext}</div>
-                    </>
-                )}
-            />
+            <Links />
+            <Route exact path="/" render={() => <h1>Home</h1>} />
+            <Route path="/new/:id" 
+                    render={({match}) => <h1>New :{match.params.id}</h1>}/>
+            <Route path="/old/:id" 
+                    render={({match}) => <Redirect to={"/new/" + match.params.id}  />}/>
         </div>
     </Router>
 );
+
 
 export default App;
